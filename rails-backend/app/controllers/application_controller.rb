6aeabbdd6ac0964baff54f5df255fb26d1b270de
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-03-04 16:20:11 -0500

    each user can delete their own comments

--- rails-backend/app/controllers/application_controller.rb
+++ rails-backend/app/controllers/application_controller.rb
@@ -10,6 +10,23 @@ class ApplicationController < ActionController::API
   # def authorized_user 
   #   render json: {error: "Not authorized"}, status: :unauthorized
   # end
+
+
+ 
+    #before_action :authenticate_user!
+  
+    private
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  
+  
+    def authenticate_user!
+      unless current_user
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+    end
+
   private
 
   def authenticate_user!
